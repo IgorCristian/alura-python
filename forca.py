4dede4cf8c938b02bd92e2980e7cbadad1213359
Igor Cristian <icdias@gmail.com>
2022-05-05 17:53:57 -0300

    Estudos dia 05/05

--- forca.py
+++ forca.py
@@ -1,8 +1,29 @@
+from operator import truediv
+
+
 def jogar():
     print("******************************************")
     print("Bem vindo ao jogo de Forca!")
     print("******************************************")
 
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+
+    #Enquanto não enforcou e não acertou...
+    while(not enforcou and not acertou):
+        
+        chute = input("Digite uma letra:")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+       
+        print("jogando ...")
     print("Fim do jogo!")
 
 if(__name__ == "__main__"):
