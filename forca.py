66d5ea3aa6af7f5e274893b9b621aebed60afbf8
IgorCristian <icdias604@gmail.com>
2022-05-05 23:22:32 -0300

    Aula em casa 05/05

--- forca.py
+++ forca.py
@@ -6,25 +6,41 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("******************************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     
     enforcou = False
     acertou = False
+    erros = 0
+    vidas = 6
 
-    #Enquanto não enforcou e não acertou...
+    print(letras_acertadas)
+
     while(not enforcou and not acertou):
         
         chute = input("Digite uma letra:")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
-       
-        print("jogando ...")
-    print("Fim do jogo!")
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            vidas -= 1
+            enforcou = erros == 6
+            print("Você só pode errar mais {} vezes".format(vidas))
+        acertou = "_" not in letras_acertadas
+
+
+        print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
 if(__name__ == "__main__"):
     jogar()
